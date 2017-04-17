b6fc3baf521d153eb06285683a6ac9ad48f61c9d
Valentin <prelovskyvalentine@gmail.com>
2017-04-17 22:56:06 +0900

    Тела функций перенесены в методы класса конвертера

--- components/Converter/ConverterUnit.pas
+++ components/Converter/ConverterUnit.pas
@@ -1,7 +1,8 @@
 unit ConverterUnit;
 
 interface
-uses  Windows, SysUtils, Classes,MyUtils,StrUtils,wdCore,getDosOutputUnit;
+uses  Windows, SysUtils, Classes,MyUtils,StrUtils,wdCore,getDosOutputUnit,
+Contnrs;
 
 procedure pdf2png(inComePDF:string);overload;
 procedure pdf2png(inComePDF,outDir:string);overload;
@@ -15,95 +16,43 @@ function convertTemp(incomePDF:string;counter:integer):string;
 function copyDoc(fromP,toP:string):string;
 function convert_docx2pdf(input:string):string;
 
-type Converter = class (TObject)
+// ----------------------------------------------------------------------
+type TDocumentConverter = class (TObject)
   public
+    Constructor Create();
+    procedure pdf2png(inComePDF:string);overload;
+    procedure pdf2png(inComePDF,outDir:string);overload;
+
+    procedure docx2png(inComeDoc:string);       overload;
+    procedure docx2png(inComeDoc,outDir:string);overload;
+
+    function convert_docx2pdf(input:string):string;
+    function copyDoc(fromP,toP:string):string;
+    function copyOrigin(originPDF,tempPDF:string):boolean;
+    function convertTemp(incomePDF:string;counter:integer):string;
+    function setOriginName(tempPNG,originPDF_ShortName:string):string;
   private
+     wd: TWdCore;
   published
 end;
-// path := C:\Program Files (x86)\gs\gs9.09\bin\
-// path gswin32 -dNOPAUSE -sDEVICE=jpeg -r150 -sOutputFile=output-%d.png midOutput.pdf
 
-var
-  dpi: string;
-  path: string;
-  Word: TWdCore;
+var dpi: string; path: string; Word: TWdCore;
 const def_path =  'C:\Program Files (x86)\gs\gs9.09\bin\';
 const tempPath = 'C:\123\pdf2 png\';
 
 implementation
 
-function convert_docx2pdf(input:string):string;
-var path,docName:string;
+Constructor TDocumentConverter.Create();
 begin
-  // docx to pdf - run by wdcore save as
-
-  word.openDoc(input);
-
-  path := ExtractFileDir(input);
-  //ShowMessage(path);
-  docName := path + '\' + ExtractFileName(input);
-  //ShowMessage(docName + ' L: ' + IntToStr(Length(docName))+ 'last = ' + IntToStr(LastCharPos(docName,'.')));
-  docName := LeftStr(docName,Length(docName)-(Length(docName)+1-LastCharPos(docName,'.'))); // 4ИС.pdf ->  4ИС
-  docName := docName + '.pdf';
-
-  // exports all pages by default + optimized for print
-//  Word.getApp.ActiveDocument.ExportAsFixedFormat(output, 17);
-  Word.getApp.ActiveDocument.ExportAsFixedFormat(docName,17);
-  Word.saveAndClose();
-  Result := docName;
+  // init word
 end;
 
-function copyDoc(fromP,toP:string):string;
-var p,p1: PAnsiChar;
-begin
-   p := PChar(fromP);
-   p1 := PChar(Concat(toP,ExtractFileName(fromP)));
-
-   CopyFile(p,p1,true);
-   Result := (p1);
-end;
-// simple copy for string
-function copyOrigin(originPDF,tempPDF:string):boolean;
-begin
-  Result := CopyFile(PChar(originPDF),PChar(tempPDF),true);;
-end;
-
-// returns temp PNG name
-function convertTemp(incomePDF:string;counter:integer):string;
-var comm:string;
-var commands: TStringList;
-begin
-   commands :=TStringList.Create;
-
-   // сама команда для посылки
-      comm :=  '"' + path +''
-      + 'gswin32" -dNOPAUSE -dBATCH -sDEVICE=jpeg -r'+Trim(dpi)
-      +' -sOutputFile="' + tempPath+IntToStr(counter)+'.png" "'
-      + Trim(incomePDF) + '"';
-  commands.Add(comm);
-  commands.SaveToFile(tempPath + 'in.bat');
-
-  getDosOutput(tempPath + 'in.bat');
-  DeleteFile(tempPath + 'in.bat');
-
-  Result := tempPath+IntToStr(counter)+'.png';
-end;
-
-function setOriginName(tempPNG,originPDF_ShortName:string):string;
-  var res:string;
-begin
-  res := ExtractFileDir(tempPNG)+'\'+originPDF_ShortName+'.png';
-  RenameFile(tempPNG,ExtractFileDir(tempPNG)+'\'+originPDF_ShortName+'.png');
-  Result := res;
-end;
-
-procedure pdf2png(inComePDF:string);overload;
+procedure TDocumentConverter.pdf2png(inComePDF:string);
 begin
   pdf2png(inComePDF,inComePDF);
 end;
-
-procedure pdf2png(inComePDF,outDir:string);overload;
-var docIn,docOut,imgIn,imgOut:string;
+procedure TDocumentConverter.pdf2png(inComePDF,outDir:string);
+  var docIn,docOut,imgIn,imgOut:string;
 k:string;
 
 begin
@@ -122,12 +71,92 @@ begin
   CopyFile(PCHar(imgOut),PChar(ExtractFileDir(outDir) + '\' + k +'.png'),true);
 end;
 
+procedure TDocumentConverter.docx2png(inComeDoc:string);begin end;
+procedure TDocumentConverter.docx2png(inComeDoc,outDir:string);begin end;
 
-procedure docx2png(inComeDoc:string);overload;
-begin end;
+function TDocumentConverter.convert_docx2pdf(input:string):string;
+var path,docName:string;
+begin
+  // docx to pdf - run by wdcore save as
 
-procedure docx2png(inComeDoc,outDir:string);overload;
-begin end;
+  word.openDoc(input);
+
+  path := ExtractFileDir(input);
+  docName := path + '\' + ExtractFileName(input);
+  docName := LeftStr(docName,Length(docName)-(Length(docName)+1
+  -LastCharPos(docName,'.'))); // 4ИС.pdf ->  4ИС
+  docName := docName + '.pdf';
+
+  // exports all pages by default + optimized for print
+  //  Word.getApp.ActiveDocument.ExportAsFixedFormat(output, 17);
+  Word.getApp.ActiveDocument.ExportAsFixedFormat(docName,17);
+  Word.saveAndClose();
+  Result := docName;
+end;
+function TDocumentConverter.copyDoc(fromP,toP:string):string;
+var p,p1: PAnsiChar;
+begin
+   p := PChar(fromP);
+   p1 := PChar(Concat(toP,ExtractFileName(fromP)));
+
+   CopyFile(p,p1,true);
+   Result := (p1);
+end;
+// simple copy for string
+function TDocumentConverter.copyOrigin(originPDF,tempPDF:string):boolean;
+begin
+  Result := CopyFile(PChar(originPDF),PChar(tempPDF),true);;
+end;
+// returns temp PNG name
+function TDocumentConverter.convertTemp(incomePDF:string;counter:integer):string;
+var comm:string;
+var commands: TStringList;
+begin
+   commands :=TStringList.Create;
+
+   // сама команда для посылки
+      comm :=  '"' + path +''
+      + 'gswin32" -dNOPAUSE -dBATCH -sDEVICE=jpeg -r'+Trim(dpi)
+      +' -sOutputFile="' + tempPath+IntToStr(counter)+'.png" "'
+      + Trim(incomePDF) + '"';
+  commands.Add(comm);
+  commands.SaveToFile(tempPath + 'in.bat');
+
+  getDosOutput(tempPath + 'in.bat');
+  DeleteFile(tempPath + 'in.bat');
+
+  Result := tempPath+IntToStr(counter)+'.png';
+end;
+function TDocumentConverter.setOriginName(tempPNG,originPDF_ShortName:string):string;
+  var res:string;
+begin
+  res := ExtractFileDir(tempPNG)+'\'+originPDF_ShortName+'.png';
+  RenameFile(tempPNG,ExtractFileDir(tempPNG)+'\'+originPDF_ShortName+'.png');
+  Result := res;
+end;
+
+
+
+
+
+
+
+
+
+// path := C:\Program Files (x86)\gs\gs9.09\bin\
+// path gswin32 -dNOPAUSE -sDEVICE=jpeg -r150 -sOutputFile=output-%d.png midOutput.pdf
+
+function convert_docx2pdf(input:string):string;begin end;
+function copyDoc(fromP,toP:string):string;begin end;
+function copyOrigin(originPDF,tempPDF:string):boolean;begin end;
+function convertTemp(incomePDF:string;counter:integer):string;begin end;
+function setOriginName(tempPNG,originPDF_ShortName:string):string;begin end;
+
+procedure pdf2png(inComePDF:string);overload;begin end;
+procedure pdf2png(inComePDF,outDir:string);overload;begin end;
+
+procedure docx2png(inComeDoc:string);overload;begin end;
+procedure docx2png(inComeDoc,outDir:string);overload;begin end;
 
 {
 procedure TForm1.Button1Click(Sender: TObject);
